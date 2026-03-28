61cac3dd735a37f1d8751bbb34f2afb0175370f2
DkevinP <maurokevin8@gmail.com>
2026-03-28 15:52:00 -0500

    Adiión de hora especifica a UTC-5

--- src/main/java/com/co/coservanguard_backend/CoservanguardBackendApplication.java
+++ src/main/java/com/co/coservanguard_backend/CoservanguardBackendApplication.java
@@ -1,7 +1,10 @@
 package com.co.coservanguard_backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class CoservanguardBackendApplication {
@@ -9,5 +12,11 @@ public class CoservanguardBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoservanguardBackendApplication.class, args);
 	}
+
+    @PostConstruct
+    public void init(){
+        // Forzar a la aplicación a usar la zona horaria de Bogotá
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Bogota"));
+    }
 
 }
